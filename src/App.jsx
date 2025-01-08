a33ddb1ca6c71195d957d0c90178b2d55078bbaf
shahid576ali <127850598+shahid576ali@users.noreply.github.com>
2025-01-08 20:36:50 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
-import Dashboard from './components/dashboard'
+import Dashboard from './components/Dashboard'
 import { Routes, Route } from 'react-router-dom'
 import Main from './pages/Main'
 const App = () => {
